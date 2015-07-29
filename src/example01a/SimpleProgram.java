cb1ba409e7aa91c8628b501dc0e58037cede3205
Douglas Hawkins <dougqh@gmail.com>
2015-07-29 16:41:07 -0700

    Adjust numbers to work better with Java 8

--- src/example01a/SimpleProgram.java
+++ src/example01a/SimpleProgram.java
@@ -1,13 +1,15 @@
 package example01a;
 
+import support.Output;
 import support.RunWith;
 
 @RunWith("-XX:+TieredCompilation")
+@Output(less=true)
 public class SimpleProgram {
   static final int CHUNK_SIZE = 1_000;
   
   public static void main(String[] args) {
-    for ( int i = 0; i < 250; ++i ) {
+    for ( int i = 0; i < 500; ++i ) {
       long startTime = System.nanoTime();
       
       for ( int j = 0; j < CHUNK_SIZE; ++j ) {
--- src/example01b/LessSimpleProgram.java
+++ src/example01b/LessSimpleProgram.java
@@ -1,15 +1,17 @@
 package example01b;
 
+import support.Output;
 import support.RunWith;
 
 @RunWith("-XX:+TieredCompilation")
+@Output(less=true)
 public class LessSimpleProgram {
   static final int CHUNK_SIZE = 1_000;
   
   public static void main(String[] args) {
     Object trap = null;
     
-    for ( int i = 0; i < 250; ++i ) {
+    for ( int i = 0; i < 1_000; ++i ) {
       long startTime = System.nanoTime();
       
       for ( int j = 0; j < CHUNK_SIZE; ++j ) {
@@ -21,7 +23,7 @@ public class LessSimpleProgram {
         }
       }
       
-      if ( i == 200 ) trap = new Object();
+      if ( i == 450 ) trap = new Object();
       
       long endTime = System.nanoTime();
       System.out.printf("%d\t%d%n", i, endTime - startTime);
